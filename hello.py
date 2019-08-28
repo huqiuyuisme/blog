bb9ac8466959856cac14537e4103914530ede11a
huqiuyuisme <1724059060@qq.com>
2019-08-28 16:49:15 +0800

    decorate

--- hello.py
+++ hello.py
@@ -1,7 +1,27 @@
+def foo(fun):
+    print("i am foo")
+    def inner(*args, **kwargs):
+        print("111111")
+        result = fun(*args, **kwargs)
+        print("222222")
+        return result
+    return inner
+
+
+def bar(fun):
+    print("i am bar")
+    def inner(*args, **kwargs):
+        print("333333")
+        result = fun(*args, **kwargs)
+        print("444444")
+        return result
+    return inner
+
+@foo
+@bar
+def fun():
+    return "i am fun "
+
 
 if __name__ == "__main__":
-    pass
-    f = lambda x:x+1
-    
-    print(type(f))
-    
+    print(fun())
